0b525300a3ab35d9a4cc952bc80b9f96393a4f2e
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2021-01-06 14:03:24 -0300

    finalizando as as rotas orders

--- backend/src/main/java/com/devnaldo/services/OrderService.java
+++ backend/src/main/java/com/devnaldo/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devnaldo.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devnaldo.dto.OrderDTO;
+import com.devnaldo.dto.ProductDTO;
 import com.devnaldo.entities.Order;
+import com.devnaldo.entities.OrderStatus;
+import com.devnaldo.entities.Product;
 import com.devnaldo.repositories.OrderRepository;
+import com.devnaldo.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
@@ -24,6 +32,20 @@ public class OrderService {
 		
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 		
+	}
+	
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+	
+			for(ProductDTO p: dto.getProducts()) {
+				Product product = productRepository.getOne(p.getId());
+				order.getProducts().add(product);
+			}
+			order =repository.save(order);
+			return new OrderDTO();
 	}
 
 }
